6c1bae6318c2a6e399ea24e8f42da2e8a062eed9
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-01-23 11:18:49 -0800

    added explanation to returned results

--- drills.js
+++ drills.js
@@ -8,7 +8,7 @@ function main() {
   let arr = new Array();
 
   // Add an item to the array
-  // arr.push(3);
+  arr.push("wendy");
   // arr.push(5);
   // arr.push(15);
   // arr.push(19);
@@ -21,14 +21,14 @@ function main() {
   // arr.push(47);
   // arr.push(30);
   // arr.push(31);
-  // console.log(arr);
+  // // console.log(arr);
   // arr.pop();
   // arr.pop();
   // arr.pop();
   // arr.pop();
   // console.log(arr);
-  arr.push(32);
-  console.log(arr);
+  // arr.push(32);
+  console.log(arr.get(0));
 }
 
 //What is the length, capacity and memory address of your array?
@@ -39,4 +39,8 @@ function main() {
 //Array {length: 9, _capacity: 12, memory: Memory, ptr: 3}
 
 //after pop x3 ==> Array {length: 9, _capacity: 39, memory: Memory, ptr: 15}
+
+// pushing in a string returns NaN - our memory data type does not accept string values
+
+//What is the purpose of the _resize() function in your Array class? It's a private function. When we run out of memory, the method makes a copy in our array that resizes the capacity of the array by a factor of 3, as determined in our code.
 main();
