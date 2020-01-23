2377af8d7cbf71dab1391b0f0b0cf932d23234de
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-01-23 12:45:50 -0800

    joined arrays

--- drills2.js
+++ drills2.js
@@ -1,12 +1,8 @@
 function urlify(string) {
-  let urlstr = '';
-  for (
-    let i = 0;
-    i < string.length;
-    i++
-  ) {
-    if (string[i] === ' ') {
-      urlstr += '%20';
+  let urlstr = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+      urlstr += "%20";
     } else {
       urlstr += string[i];
     }
@@ -28,19 +24,49 @@ function flessthan5(arr) {
   }
   return filtered;
 }
-console.log(
-  flessthan5([
-    1,
-    2,
-    5,
-    3,
-    5,
-    476,
-    4,
-    100000234234,
-    6,
-    -1,
-    -70,
-    0
-  ])
-);
+// console.log(flessthan5([1, 2, 5, 3, 5, 476, 4, 100000234234, 6, -1, -70, 0]));
+
+function sumUp(arr) {
+  let currentMax = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum > currentMax) {
+        console.log(currentMax);
+        currentMax = sum;
+      }
+    }
+    sum = 0;
+  }
+  return currentMax;
+}
+// console.log(sumUp([4, 6, -3, 5, -2, 1, 10]));
+
+function joinArrays(arr1, arr2) {
+  //check both arrays at idx 0; take lowest and add to new array
+  let i = 0;
+  let j = 0;
+  let joinedArr = [];
+  let longArr = arr1.length > arr2.length ? arr1 : arr2;
+  let shortArr = arr1.length > arr2.length ? arr2 : arr1;
+  while (j <= shortArr.length) {
+    if (longArr[i] > shortArr[j]) {
+      joinedArr.push(shortArr[j]);
+      j++;
+    }
+    if (shortArr[j] > longArr[i]) {
+      joinedArr.push(longArr[i]);
+      i++;
+    }
+    if (shortArr[j] === longArr[i]) {
+      joinedArr.push(shortArr[j]);
+      joinedArr.push(longArr[i]);
+      i++;
+      j++;
+    }
+  }
+  return joinedArr;
+}
+
+console.log(joinArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
