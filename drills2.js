1a20b3112ea2e4fc627aec0ca1ca148217450cba
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-01-23 14:24:45 -0800

    adding 2D zero

--- drills2.js
+++ drills2.js
@@ -1,12 +1,8 @@
 function urlify(string) {
-  let urlstr = '';
-  for (
-    let i = 0;
-    i < string.length;
-    i++
-  ) {
-    if (string[i] === ' ') {
-      urlstr += '%20';
+  let urlstr = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+      urlstr += "%20";
     } else {
       urlstr += string[i];
     }
@@ -34,11 +30,7 @@ function sumUp(arr) {
   let currentMax = arr[0];
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    for (
-      let j = i;
-      j < arr.length;
-      j++
-    ) {
+    for (let j = i; j < arr.length; j++) {
       sum += arr[j];
       if (sum > currentMax) {
         console.log(currentMax);
@@ -56,35 +48,17 @@ function joinArrays(arr1, arr2) {
   let i = 0;
   let j = 0;
   let joinedArr = [];
-  let longArr =
-    arr1.length >= arr2.length
-      ? arr1
-      : arr2;
-  let shortArr =
-    arr1.length >= arr2.length
-      ? arr2
-      : arr1;
+  let longArr = arr1.length >= arr2.length ? arr1 : arr2;
+  let shortArr = arr1.length >= arr2.length ? arr2 : arr1;
 
-  while (
-    i < longArr.length ||
-    j < shortArr.length
-  ) {
-    if (
-      longArr[i] > shortArr[j] ||
-      longArr.length === i
-    ) {
+  while (i < longArr.length || j < shortArr.length) {
+    if (longArr[i] > shortArr[j] || longArr.length === i) {
       joinedArr.push(shortArr[j]);
       j++;
-    } else if (
-      shortArr[j] > longArr[i] ||
-      shortArr.length === j
-    ) {
+    } else if (shortArr[j] > longArr[i] || shortArr.length === j) {
       joinedArr.push(longArr[i]);
       i++;
-    } else if (
-      shortArr[j] === longArr[i] &&
-      shortArr[j]
-    ) {
+    } else if (shortArr[j] === longArr[i] && shortArr[j]) {
       joinedArr.push(shortArr[j]);
       joinedArr.push(longArr[i]);
       i++;
@@ -102,19 +76,11 @@ function joinArrays(arr1, arr2) {
 // );
 
 function removechar(str, remove) {
-  let result = '';
+  let result = "";
 
-  for (
-    let j = 0;
-    j < remove.length;
-    j++
-  ) {
-    result = '';
-    for (
-      let i = 0;
-      i < str.length;
-      i++
-    ) {
+  for (let j = 0; j < remove.length; j++) {
+    result = "";
+    for (let i = 0; i < str.length; i++) {
       if (str[i] !== remove[j]) {
         result += str[i];
       }
@@ -127,3 +93,49 @@ function removechar(str, remove) {
 // console.log(
 //   removechar('Danaeioua', 'aeiou')
 // );
+
+function products(arr) {
+  let prodArr = [];
+  let prod = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (j !== i) {
+        prod *= arr[j];
+      }
+    }
+    prodArr.push(prod);
+    prod = 1;
+  }
+  return prodArr;
+}
+
+// console.log(products([1, 3, 9, 4]));
+
+let matrix = [
+  [1, 0, 1, 1, 0],
+  [0, 1, 1, 1, 0],
+  [1, 1, 1, 1, 1],
+  [1, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1]
+];
+
+function zeroMeOut(arr) {
+  let copy = arr;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      // i transverses row, j the columns
+      if (arr[i][j] === 0) {
+        for (let k = 0; k < arr[i].length; k++) {
+          copy[i][k] = 2;
+        }
+        for (let k = 0; k < arr.length; k++) {
+          copy[k][j] = 0;
+        }
+      }
+    }
+  }
+  return copy;
+}
+
+console.log(zeroMeOut(matrix));
