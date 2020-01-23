3439951454ea5a938aeca596189a0efb21513b92
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-23 13:32:38 -0800

    wrote a function to remove chars from an array, and another that merges two arrays that are presorted

--- drills2.js
+++ drills2.js
@@ -1,8 +1,12 @@
 function urlify(string) {
-  let urlstr = "";
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === " ") {
-      urlstr += "%20";
+  let urlstr = '';
+  for (
+    let i = 0;
+    i < string.length;
+    i++
+  ) {
+    if (string[i] === ' ') {
+      urlstr += '%20';
     } else {
       urlstr += string[i];
     }
@@ -30,7 +34,11 @@ function sumUp(arr) {
   let currentMax = arr[0];
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
+    for (
+      let j = i;
+      j < arr.length;
+      j++
+    ) {
       sum += arr[j];
       if (sum > currentMax) {
         console.log(currentMax);
@@ -48,18 +56,35 @@ function joinArrays(arr1, arr2) {
   let i = 0;
   let j = 0;
   let joinedArr = [];
-  let longArr = arr1.length > arr2.length ? arr1 : arr2;
-  let shortArr = arr1.length > arr2.length ? arr2 : arr1;
-  while (j <= shortArr.length) {
-    if (longArr[i] > shortArr[j]) {
+  let longArr =
+    arr1.length >= arr2.length
+      ? arr1
+      : arr2;
+  let shortArr =
+    arr1.length >= arr2.length
+      ? arr2
+      : arr1;
+
+  while (
+    i < longArr.length ||
+    j < shortArr.length
+  ) {
+    if (
+      longArr[i] > shortArr[j] ||
+      longArr.length === i
+    ) {
       joinedArr.push(shortArr[j]);
       j++;
-    }
-    if (shortArr[j] > longArr[i]) {
+    } else if (
+      shortArr[j] > longArr[i] ||
+      shortArr.length === j
+    ) {
       joinedArr.push(longArr[i]);
       i++;
-    }
-    if (shortArr[j] === longArr[i]) {
+    } else if (
+      shortArr[j] === longArr[i] &&
+      shortArr[j]
+    ) {
       joinedArr.push(shortArr[j]);
       joinedArr.push(longArr[i]);
       i++;
@@ -69,4 +94,36 @@ function joinArrays(arr1, arr2) {
   return joinedArr;
 }
 
-console.log(joinArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
+// console.log(
+//   joinArrays(
+//     [1, 3, 6, 8, 11],
+//     [2, 3, 5, 8, 9, 10]
+//   )
+// );
+
+function removechar(str, remove) {
+  let result = '';
+
+  for (
+    let j = 0;
+    j < remove.length;
+    j++
+  ) {
+    result = '';
+    for (
+      let i = 0;
+      i < str.length;
+      i++
+    ) {
+      if (str[i] !== remove[j]) {
+        result += str[i];
+      }
+    }
+    console.log(result);
+    str = result;
+  }
+  return str;
+}
+// console.log(
+//   removechar('Danaeioua', 'aeiou')
+// );
