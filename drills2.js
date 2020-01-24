fb5e78ca3d4391f9696786d50d6a313f38d28807
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2020-01-23 16:19:50 -0800

    finished the rotation drill and fixed array copy bug

--- drills2.js
+++ drills2.js
@@ -1,8 +1,12 @@
 function urlify(string) {
-  let urlstr = "";
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === " ") {
-      urlstr += "%20";
+  let urlstr = '';
+  for (
+    let i = 0;
+    i < string.length;
+    i++
+  ) {
+    if (string[i] === ' ') {
+      urlstr += '%20';
     } else {
       urlstr += string[i];
     }
@@ -30,7 +34,11 @@ function sumUp(arr) {
   let currentMax = arr[0];
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
+    for (
+      let j = i;
+      j < arr.length;
+      j++
+    ) {
       sum += arr[j];
       if (sum > currentMax) {
         console.log(currentMax);
@@ -48,17 +56,35 @@ function joinArrays(arr1, arr2) {
   let i = 0;
   let j = 0;
   let joinedArr = [];
-  let longArr = arr1.length >= arr2.length ? arr1 : arr2;
-  let shortArr = arr1.length >= arr2.length ? arr2 : arr1;
+  let longArr =
+    arr1.length >= arr2.length
+      ? arr1
+      : arr2;
+  let shortArr =
+    arr1.length >= arr2.length
+      ? arr2
+      : arr1;
 
-  while (i < longArr.length || j < shortArr.length) {
-    if (longArr[i] > shortArr[j] || longArr.length === i) {
+  while (
+    i < longArr.length ||
+    j < shortArr.length
+  ) {
+    if (
+      longArr[i] > shortArr[j] ||
+      longArr.length === i
+    ) {
       joinedArr.push(shortArr[j]);
       j++;
-    } else if (shortArr[j] > longArr[i] || shortArr.length === j) {
+    } else if (
+      shortArr[j] > longArr[i] ||
+      shortArr.length === j
+    ) {
       joinedArr.push(longArr[i]);
       i++;
-    } else if (shortArr[j] === longArr[i] && shortArr[j]) {
+    } else if (
+      shortArr[j] === longArr[i] &&
+      shortArr[j]
+    ) {
       joinedArr.push(shortArr[j]);
       joinedArr.push(longArr[i]);
       i++;
@@ -76,11 +102,19 @@ function joinArrays(arr1, arr2) {
 // );
 
 function removechar(str, remove) {
-  let result = "";
+  let result = '';
 
-  for (let j = 0; j < remove.length; j++) {
-    result = "";
-    for (let i = 0; i < str.length; i++) {
+  for (
+    let j = 0;
+    j < remove.length;
+    j++
+  ) {
+    result = '';
+    for (
+      let i = 0;
+      i < str.length;
+      i++
+    ) {
       if (str[i] !== remove[j]) {
         result += str[i];
       }
@@ -98,7 +132,11 @@ function products(arr) {
   let prodArr = [];
   let prod = 1;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
+    for (
+      let j = 0;
+      j < arr.length;
+      j++
+    ) {
       if (j !== i) {
         prod *= arr[j];
       }
@@ -120,22 +158,75 @@ let matrix = [
 ];
 
 function zeroMeOut(arr) {
-  let copy = arr;
+  let copy = [];
+  arr.forEach((e, i) => {
+    copy[i] = [...e];
+  });
+  // console.log(arr, copy);
+  console.log(arr[0] === copy[0]);
+  console.log(arr === copy);
 
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
+    for (
+      let j = 0;
+      j < arr[i].length;
+      j++
+    ) {
       // i transverses row, j the columns
       if (arr[i][j] === 0) {
-        for (let k = 0; k < arr[i].length; k++) {
-          copy[i][k] = 2;
-        }
-        for (let k = 0; k < arr.length; k++) {
+        for (
+          let k = 0;
+          k < arr.length;
+          k++
+        ) {
           copy[k][j] = 0;
         }
       }
     }
   }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(0)) {
+      copy[i] = copy[i].map(x => 0);
+    }
+  }
   return copy;
 }
+// console.log(matrix);
+// console.log(zeroMeOut(matrix));
 
-console.log(zeroMeOut(matrix));
+function rotation(string, rotation) {
+  if (
+    string.length !== rotation.length
+  ) {
+    return false;
+  }
+  let count = 0;
+  let r = 0;
+
+  for (
+    let i = 0;
+    i < string.length;
+    i++
+  ) {
+    console.log(i, r);
+    if (string[i] === rotation[r]) {
+      console.log(i);
+      if (i === string.length - 1) {
+        return true;
+      }
+    } else {
+      count++;
+      if (count === string.length) {
+        return false;
+      }
+      i = 0;
+    }
+    r++;
+    if (r === rotation.length) {
+      r = 0;
+    }
+  }
+}
+console.log(
+  rotation('amazon', 'azonma')
+);
